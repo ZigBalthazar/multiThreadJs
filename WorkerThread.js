8f04cac9a815928b498abc12b22e39de66024c41
pouya <pouya@radhoosh.ir>
2023-01-10 15:44:04 +0330

    send message to worker

--- WorkerThread.js
+++ WorkerThread.js
@@ -3,7 +3,7 @@ const {Worker,isMainThread } =require('worker_threads');
 
 if (isMainThread) {
     console.log("Main Thread")
-    new Worker('./worker.js')
+    const newWorker = new Worker('./worker.js').postMessage("message From Main")
 } else {
-    
+    console.log("its main thread!!!")
 }
--- worker.js
+++ worker.js
@@ -1,1 +1,5 @@
-console.log("Hi! Im Worker.")
+const {parentPort} = require('worker_threads');
+
+parentPort.on('message',(msg)=>{
+    console.log(`Hi! Im Worker and its a msg from main => ${msg}`)
+})
